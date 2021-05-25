32ce263533f962409014da81e167ff16cb3021f4
heonampyo <huhnampyo@gmail.com>
2021-05-25 20:42:12 +0900

    Initial Commit

---
+++ DaitsoiOSProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DaitsoiOSProject
+//
+//  Created by 허남표 on 2021/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DaitsoiOSProject/DaitsoiOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaitsoiOSProjectApp.swift
+//  DaitsoiOSProject
+//
+//  Created by 허남표 on 2021/05/25.
+//
+
+import SwiftUI
+
+@main
+struct DaitsoiOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
